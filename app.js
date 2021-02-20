35e0d67500a9628dae922ed75308a92a358c3e5e
Gabriel Ballester <gaby.ballester@gmail.com>
2021-02-20 16:44:50 +0100

    Configuración inicial + librerías base + mongodb conectada

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const { API_VERSION } = require("./config");
+
+// Load routings
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Configure Header HTTP (CORS)
+// ....
+
+// Router Basic
+// ....
+
+module.exports = app;
---
+++ config.js
@@ -0,0 +1,13 @@
+const API_VERSION = "v1";
+const IP_SERVER = "localhost";
+const port = process.env.PORT || 3977;
+const PORT_DB = 27017;
+const DB_NAME = "web-personal";
+
+module.exports = {
+  API_VERSION,
+  IP_SERVER,
+  port,
+  PORT_DB,
+  DB_NAME
+};
