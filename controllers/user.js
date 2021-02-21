f748b368f1dd501b4ead97a4d64c7814cf078901
Gabriel Ballester <gaby.ballester@gmail.com>
2021-02-21 01:30:39 +0100

    Registro de usuario

--- controllers/user.js
+++ controllers/user.js
@@ -7,47 +7,46 @@ const User = require("../models/user");
 
 // función de registro
 function signUp(req, res) {
-  console.log("endpoint de signup");
+
+  const user = new User();
+
+  const { name, lastname, email, password, repeatPassword } = req.body;
+  user.name = name;
+  user.lastname = lastname;
+  user.email = email.toLowerCase();
+  user.role = "admin";
+  user.active = false;
+
+  if (!password || !repeatPassword) {
+    res.status(404).send({ message: "Las contraseñas son obligatorias." });
+  } else {
+    console.log('continuar');
+    if (password !== repeatPassword) {
+      res.status(404).send({ message: "Las contraseñas no son iguales." });
+    } else {
+      //encriptación
+      bcrypt.hash(password, null, null, function (err, hash) {
+        if (err) {
+          res.status(500).send({ message: "Error al encriptar la contraseña." });
+        } else {
+          user.password = hash;
+          // guardado
+          user.save((err, userStored) => {
+            if (err) {
+              res.status(500).send({ message: "El usuario ya existe." });
+            } else {
+              if (!userStored) {
+                res.status(404).send({ message: "Error al crear el usuario." });
+              } else {
+                res.status(200).send({ user: userStored });
+              }
+            }
+          });
+        }
+      });
+    }
+  }
 }
-//   const user = new User();
-
-//   const { name, lastname, email, password, repeatPassword } = req.body;
-//   user.name = name;
-//   user.lastname = lastname;
-//   user.email = email.toLowerCase();
-//   user.role = "admin";
-//   user.active = false;
-
-//   if (!password || !repeatPassword) {
-//     res.status(404).send({ message: "Las contraseñas son obligatorias." });
-//   } else {
-//     if (password !== repeatPassword) {
-//       res.status(404).send({ message: "Las contraseñas no son iguales." });
-//     } else {
-//       bcrypt.hash(password, null, null, function(err, hash) {
-//         if (err) {
-//           res
-//             .status(500)
-//             .send({ message: "Error al encriptar la contraseña." });
-//         } else {
-//           user.password = hash;
-
-//           user.save((err, userStored) => {
-//             if (err) {
-//               res.status(500).send({ message: "El usuario ya existe." });
-//             } else {
-//               if (!userStored) {
-//                 res.status(404).send({ message: "Error al crear el usuario." });
-//               } else {
-//                 res.status(200).send({ user: userStored });
-//               }
-//             }
-//           });
-//         }
-//       });
-//     }
-//   }
-// }
 
 // function signIn(req, res) {
 //   const params = req.body;
@@ -283,13 +282,13 @@ function signUp(req, res) {
 
 module.exports = {
   signUp,
-//   signIn,
-//   getUsers,
-//   getUsersActive,
-//   uploadAvatar,
-//   getAvatar,
-//   updateUser,
-//   activateUser,
-//   deleteUser,
-//   signUpAdmin
+  //   signIn,
+  //   getUsers,
+  //   getUsersActive,
+  //   uploadAvatar,
+  //   getAvatar,
+  //   updateUser,
+  //   activateUser,
+  //   deleteUser,
+  //   signUpAdmin
 };
