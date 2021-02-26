d48d105a506f65c4912144e71632161772d20613
Gabriel Ballester <gaby.ballester@gmail.com>
2021-02-26 13:35:03 +0100

    Borrar usuario

--- controllers/user.js
+++ controllers/user.js
@@ -217,6 +217,24 @@ function activateUser(req, res) {
   });
 }
 
+function deleteUser(req, res) {
+  const { id } = req.params;
+
+  User.findByIdAndRemove(id, (err, userDeleted) => {
+    if (err) {
+      res.status(500).send({ message: "Error del servidor." });
+    } else {
+      if (!userDeleted) {
+        res.status(404).send({ message: "Usuario no encontrado." });
+      } else {
+        res
+          .status(200)
+          .send({ message: "El usuario ha sido eliminado correctamente." });
+      }
+    }
+  });
+}
+
 module.exports = {
   signUp,
   signIn,
@@ -225,5 +243,6 @@ module.exports = {
   uploadAvatar,
   getAvatar,
   updateUser,
-  activateUser
+  activateUser,
+  deleteUser
 };
