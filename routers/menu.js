07ddc8081ace2f4891a0076cb459796e94e3d1b0
Gabriel Ballester <gaby.ballester@gmail.com>
2021-02-28 21:42:17 +0100

    Función para actualizar menú web

--- routers/menu.js
+++ routers/menu.js
@@ -7,5 +7,6 @@ const api = express.Router();
 
 api.post("/add-menu", [md_auth.ensureAuth], MenuController.addMenu);
 api.get("/get-menus", MenuController.getMenus);
+api.put("/update-menu/:id", [md_auth.ensureAuth], MenuController.updateMenu);
 
 module.exports = api;
