d86b19d38234ffd0e8af174566e964c254aa10a6
Gabriel Ballester <gaby.ballester@gmail.com>
2021-03-01 21:17:30 +0100

    Obtener cursos

--- controllers/course.js
+++ controllers/course.js
@@ -24,7 +24,26 @@ function addCourse(req, res) {
   });
 }
 
+function getCourses(req, res) {
+  Course.find()
+    .sort({ order: "asc" })
+    .exec((err, coursesStored) => {
+      if (err) {
+        res.status(500).send({ code: 500, message: "Error del servidor." });
+      } else {
+        if (!coursesStored) {
+          res
+            .status(404)
+            .send({ code: 404, message: "No se ha encontrado ningun curso." });
+        } else {
+          res.status(200).send({ code: 200, courses: coursesStored });
+        }
+      }
+    });
+}
+
 
 module.exports = {
-  addCourse
+  addCourse,
+  getCourses
 };
