6bcc13e9436068c6f5e46cdbe994fa56a3f6f1e5
Gabriel Ballester <gaby.ballester@gmail.com>
2021-03-01 21:13:13 +0100

    Añadir cursos

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const authRoutes = require("./routers/auth");
 const userRoutes = require("./routers/user");
 const menuRoutes = require("./routers/menu");
 const newsletterRoutes = require("./routers/newsletter");
+const courseRoutes = require("./routers/course");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
 app.use(`/api/${API_VERSION}`, authRoutes);
 app.use(`/api/${API_VERSION}`, userRoutes);
 app.use(`/api/${API_VERSION}`, menuRoutes);
+app.use(`/api/${API_VERSION}`, courseRoutes);
 app.use(`/api/${API_VERSION}`, newsletterRoutes);
 
 module.exports = app;
---
+++ controllers/course.js
@@ -0,0 +1,30 @@
+const Course = require("../models/course");
+
+function addCourse(req, res) {
+  const body = req.body;
+  const course = new Course(body);
+  course.order = 1000;
+
+  course.save((err, courseStored) => {
+    if (err) {
+      res
+        .status(400)
+        .send({ code: 400, message: "El curso que estás creando ya existe." });
+    } else {
+      if (!courseStored) {
+        res
+          .status(400)
+          .send({ code: 400, message: "No se ha podido crear el curso." });
+      } else {
+        res
+          .status(200)
+          .send({ code: 200, message: "Curso creado correctamente." });
+      }
+    }
+  });
+}
+
+
+module.exports = {
+  addCourse
+};
